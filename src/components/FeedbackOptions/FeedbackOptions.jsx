43de6ffaa7ff0fe11170436b1489e09404b31eeb
OLEH FEDORCHUK <fedorchuk2018@gmail.com>
2023-07-24 00:02:32 +0100

    update

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -3,7 +3,6 @@ import React from "react";
 const FeedbackOptions = ({ options, onLeaveFeedback }) => {
   return (
     <div>
-      <h3>Please leave feedback</h3>
       {options.map((option) => (
         <button key={option} onClick={() => onLeaveFeedback(option)}>
           {option}
