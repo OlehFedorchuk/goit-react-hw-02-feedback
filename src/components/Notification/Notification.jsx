dc11a8eeed12ae99c39257cb1aa3d4bdee76379c
OLEH FEDORCHUK <fedorchuk2018@gmail.com>
2023-07-24 09:37:33 +0100

    update

--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const Notification = ({ message }) => {
   return (
@@ -8,4 +9,7 @@ const Notification = ({ message }) => {
   );
 };
 
+Notification.propTypes = {
+  message: PropTypes.string.isRequired,
+}
 export default Notification;
